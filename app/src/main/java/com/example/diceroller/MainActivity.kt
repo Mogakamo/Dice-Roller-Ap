3e003d16a955245d5443a1db92ea3267d6c77865
Mogakamo <mogaka.amo254@gmail.com>
2021-07-01 15:36:51 +0300

    add the private roll function

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +11,13 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton : Button = findViewById(R.id.button)
 
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = "6"
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        TODO("Not yet implemented")
     }
 }
 
