3766129575fda57b2c8deb52f42f18e5bbbc3943
Mogakamo <mogaka.amo254@gmail.com>
2021-07-01 16:23:02 +0300

    added the dice code in MainActivity.kt

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
@@ -31,8 +31,8 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
 
         //update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
