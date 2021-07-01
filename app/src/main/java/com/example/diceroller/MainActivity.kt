c49029280509931e81c0ae17cb3472e9d695a6f4
Mogakamo <mogaka.amo254@gmail.com>
2021-07-01 15:31:35 +0300

    added the code for the textView

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton : Button = findViewById(R.id.button)
 
-
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = "6"
     }
 }
